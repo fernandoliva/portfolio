97052fec92d47f43d8ebcf2dff382147c2b5bbee
Fernando de la Oliva <fernandoliva9@gmail.com>
2021-12-27 10:49:11 +0100

    fix text center about

--- src/partials/About.js
+++ src/partials/About.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function About({ title, description }) {
   return (
-    <div className="max-w-4xl mx-8 mt-16">
+    <div className="max-w-4xl md:mx-auto mx-8 mt-16">
       <p className="text-2xl md:text-4xl font-bold text-center">{title}</p>
       {description.map((item, index) => (
         <p key={index} className="text-base text-justify md:text-center text-gray-600 leading-relaxed mt-4">
