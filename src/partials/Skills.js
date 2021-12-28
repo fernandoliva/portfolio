3dcb0c4416d88b06ab40f4abf52f4cbf915d999c
Fernando de la Oliva <fernandoliva9@gmail.com>
2021-12-28 01:42:06 +0100

    fixed flex skills

--- src/partials/Skills.js
+++ src/partials/Skills.js
@@ -3,7 +3,7 @@ import SkillCard from "./SkillCard";
 
 function Skills({ skills }) {
   return (
-    <div className="grid md:grid-cols-3 xs:grid-cols-2 align-center justify-center max-w-2xl mx-auto mt-8">
+    <div className="flex flex-row flex-wrap align-center justify-center max-w-xl mx-auto mt-8">
       {skills.map((skill,i) => {
         return (
           <SkillCard skillName={skill.skillName} skillIcon={skill.skillIcon} skillColor={skill.color} value={{color: skill.color}} key={i} />
