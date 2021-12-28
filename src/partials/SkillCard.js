66b9b7be2c95937d9691209f1bca29ab35e15cb5
Fernando de la Oliva <fernandoliva9@gmail.com>
2021-12-28 01:49:22 +0100

    animation on skills

--- src/partials/SkillCard.js
+++ src/partials/SkillCard.js
@@ -1,8 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const SkillCard = ({ skillName, skillIcon, skillColor }) => {
+  useEffect(() => {
+    AOS.init({
+      once: true,
+    });
+  });
   return (
-    <div className="mt-5 w-40 flex flex-col align-middle justify-center mx-auto text-center p-5 rounded-3xl border-2 border-gray-300" style={{ color: skillColor}}>
+    <div className="mt-5 w-40 flex flex-col align-middle justify-center mx-auto text-center p-5 rounded-3xl border-2 border-gray-300" style={{ color: skillColor}} data-aos="fade-up" data-aos-duration="1500">
       {skillIcon}
       <p className="text-xl font-semibold mt-4 text-black">{skillName}</p>
     </div>
--- src/partials/Skills.js
+++ src/partials/Skills.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useEffect } from "react";
 import SkillCard from "./SkillCard";
+
 
 function Skills({ skills }) {
   return (
