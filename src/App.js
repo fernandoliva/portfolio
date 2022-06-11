af087d83de97a7ad98201c58d2c92d20d90fbf2f
Fernando <fernandoliva9@gmail.com>
2022-06-11 04:03:51 +0200

    fix parallax

--- src/App.js
+++ src/App.js
@@ -20,8 +20,8 @@ function App() {
       <div data-aos="fade-down" data-aos-duration="800">
         <Card name={data.name} title={data.title} social={data.social} />
       </div>
+      <About title={data.about.title} description={data.about.description} />
       <div data-aos="fade-up" data-aos-duration="800" data-aos-delay="400">
-        <About title={data.about.title} description={data.about.description} />
         <Skills skills={data.skills} />
         <Projects projects={data.projects} />
         <Footer />
--- src/partials/About.js
+++ src/partials/About.js
@@ -1,21 +1,32 @@
 import React from "react";
-import bgImage from "./../images/aboutme__bg.webp";
+import bgImage from "./../images/aboutme__bg2.webp";
 
 function About({ title, description }) {
   const parallax = {
     backgroundImage: `url(${bgImage})`,
   }
   return (
-    <section className="container flex items-center justify-center h-fit my-20 p-10 bg-fixed bg-center bg-cover custom-img mx-auto rounded-xl max-w-4xl md:mx-auto text-2xl text-slate-700" style={parallax}>
-        <div className="">
-          <div className="about"  />
-          <p className="text-2xl md:text-4xl font-bold text-center">{title}</p>
+    // <section className="container flex items-center justify-center h-fit my-20 p-10 bg-fixed bg-center bg-cover custom-img mx-auto rounded-xl max-w-4xl md:mx-auto text-2xl text-slate-700" style={parallax}>
+    //     <div className="">
+    //       <div className="about"  />
+    //       <p className="text-2xl md:text-4xl font-bold text-center">{title}</p>
+    //       {description.map((item, index) => (
+    //         <p key={index} className="text-base text-justify md:text-center text-slate-600 leading-relaxed mt-4">
+    //           {item}
+    //         </p>
+    //       ))}
+    // </div>
+    // </section>
+    <section className="flex items-center justify-center h-fit my-20 p-10 bg-fixed bg-contain bg-left-top custom-img mx-auto rounded-xl max-w-3xl md:mx-auto text-2xl text-slate-700" style={parallax}>
+      <div>
+          <p className="text-2xl text-white md:text-4xl font-bold text-center">{title}</p>
           {description.map((item, index) => (
-            <p key={index} className="text-base text-justify md:text-center text-slate-600 leading-relaxed mt-4">
+            <p key={index} className="text-base text-white text-justify md:text-center text-slate-600 leading-relaxed mt-4">
               {item}
             </p>
           ))}
-    </div>
+
+      </div>
     </section>
   );
 }
